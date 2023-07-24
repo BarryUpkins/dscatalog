4dbe98dbcaeb537c81e8a88de7cfd0f015c3d12a
BarryUpkins <pk1st.backup@gmail.com>
2023-07-24 19:09:25 -0300

    ORM mapping and db seeding

--- backend/src/main/java/com/expert/dscatalog/entities/Category.java
+++ backend/src/main/java/com/expert/dscatalog/entities/Category.java
@@ -5,7 +5,9 @@ import jakarta.persistence.*;
 import java.io.Serial;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table( name = "tb_category")
@@ -23,6 +25,9 @@ public class Category implements Serializable {
 
     @Column( columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
+
+    @ManyToMany( mappedBy = "categories" )
+    private Set<Product> products = new HashSet<>();
 
     public Category() { }
 
@@ -53,6 +58,10 @@ public class Category implements Serializable {
 
     public Instant getUpdatedAt() {
         return updatedAt;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @PrePersist
